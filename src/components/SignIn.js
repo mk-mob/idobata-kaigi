d6620d5cfff85cceda58def5e6eb1ad8bb857440
mk-mob <mkmob314@gmail.com>
2021-03-03 18:46:39 +0900

    add clicking button

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -42,8 +42,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function SignIn({setName}) {
   const classes = useStyles();
+  const [disabled, setDisabled] = useState(true);
+  const [string, setString] = useState('');
+  console.log(string);
+  useEffect(() => {
+    const disabled = string === '';
+    setDisabled(disabled);
+  },[string]);
 
   return (
     <Container component="main" maxWidth="xs">
@@ -63,15 +70,17 @@ export default function SignIn() {
             label="ニックネーム"
             name="name"
             autoFocus
+            onChange ={e => setString(e.target.value)}
           />
          
          
           <Button
-            type="submit"
+            type="submit" 
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            disabled ={disabled}
           >
             はじめる
           </Button>

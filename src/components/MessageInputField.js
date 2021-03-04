55b5a926d76ac051160e8be70ca0ce8d84d5b851
mk-mob <mkmob314@gmail.com>
2021-03-04 15:18:14 +0900

    add inputfield style

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        gridRow: 2,
+
+    }
+});
 
 const MessageInputField = () => {
-
-    return <div>MessageInputField</div>;
+    const classes = useStyles();
+    return <div className ={classes.root} >
+        
+        
+        
+        </div>;
 
 };  
 
